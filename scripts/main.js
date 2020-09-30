acfeeff23fb40484aa178007fda00c353d967b6f
MariiaRev <maria.revenko2209@gmail.com>
2020-09-30 16:55:07 +0300

    edited open submenu by clicking catalog list, implemented close submenu

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
+'use strict';   //on the beginning of a function (or doc because it's a function too)
+                //means "write code right. not a = 5 (undefined) but let (or const) a = 5"
+
 const btnBurger = document.querySelector('.btn-burger');
 const catalog = document.querySelector('.catalog');
 const overlay = document.querySelector('.overlay');
 const btnClose = document.querySelector('.btn-close');
 const subCatalog = document.querySelector('.subcatalog');
 const subCatalogHeader = document.querySelector('.subcatalog-header');
-
+const btnReturn = document.querySelector('.btn-return');
 
 const openMenu = () => {
     catalog.classList.add('open');
@@ -14,6 +17,7 @@ const openMenu = () => {
 const closeMenu = () => {
     catalog.classList.remove('open');
     overlay.classList.remove('active');
+    closeSubMenu();
 };
 
 const openSubMenu = (event) => {
@@ -26,11 +30,15 @@ const openSubMenu = (event) => {
     }
 };
 
+const closeSubMenu = () => {
+    subCatalog.classList.remove('subopen');
+};
+
 btnBurger.addEventListener('click', openMenu);      //open menu by clicking btn-burger
 btnClose.addEventListener('click', closeMenu);      //close menu by clicking btn-close
 overlay.addEventListener('click', closeMenu);       //close menu by clicking on overlay
-catalog.addEventListener('click', openSubMenu); //open submenu by clicking catalog list
-
+catalog.addEventListener('click', openSubMenu);     //open submenu by clicking catalog list
+btnReturn.addEventListener('click', closeSubMenu);  //close submenu by clicking btn-return
 
 
 
